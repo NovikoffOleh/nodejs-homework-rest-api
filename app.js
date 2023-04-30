89902f378e2a2a86836278114b1837aee9f8bb4c
NovikoffOleh <novikoff10000@gmail.com>
2023-04-30 14:04:35 +0300

    upd files

--- app.js
+++ app.js
@@ -1,54 +1,37 @@
-/*const express = require('express')
-const logger = require('morgan')
-const cors = require('cors')
+const express = require("express");
+const logger = require("morgan");
+const cors = require("cors");
 
-const contactsRouter = require('./routes/api/contacts')
+const mongoose = require("mongoose");
 
-const app = express()
+require("dotenv").config();
 
-const formatsLogger = app.get('env') === 'development' ? 'dev' : 'short'
 
-app.use(logger(formatsLogger))
-app.use(cors())
-app.use(express.json())
+const { DB_HOST } = process.env;
+mongoose
+  .connect(DB_HOST)
+  .then(() => console.log("Database connects success"))
+  .catch((e) => console.log(e));
 
-app.use('/api/contacts', contactsRouter)
+const contactsRouter = require("./routes/api/contacts");
 
-app.use((req, res) => {
-  res.status(404).json({ message: 'Not found' })
-})
+const app = express();
 
-app.use((err, req, res, next) => {
-  const {status = 500, message = "Server error"}= err;
-  res.status(status).json({message});
-  
-  //res.status(500).json({ message: err.message })
-})
+const formatsLogger = app.get("env") === "development" ? "dev" : "short";
 
-module.exports = app*/
-const express = require('express')
-const logger = require('morgan')
-const cors = require('cors')
+app.use(logger(formatsLogger));
+app.use(cors());
+app.use(express.json());
 
-const contactsRouter = require('./routes/api/contacts')
-
-const app = express()
-
-const formatsLogger = app.get('env') === 'development' ? 'dev' : 'short'
-
-app.use(logger(formatsLogger))
-app.use(cors())
-app.use(express.json())
-
-app.use('/api/contacts', contactsRouter)
+app.use("/api/contacts", contactsRouter);
 
 app.use((req, res) => {
-  res.status(404).json({ message: 'Not found' })
-})
-
-app.use((err, req, res, next) => {
-  const { status = 500, message = "Server error" } = err;
-  res.status(status).json({ message });
+  res.status(404).json({ message: "Not found" });
 });
 
-module.exports = app
+app.use((err, req, res, next) => {
+  const {status = 500, message = "Server error"} = err;
+  res.status(status).json({ message, })
+})
+
+module.exports = app;
