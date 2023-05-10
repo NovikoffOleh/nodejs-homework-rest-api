c5ec415af745250e2107667b1e29c44188bc542a
NovikoffOleh <novikoff10000@gmail.com>
2023-05-10 17:47:20 +0300

    upd files

--- models/contact.js
+++ models/contact.js
@@ -1,50 +1,57 @@
-
-const { Schema, model } = require("mongoose");
+const {Schema,model} = require("mongoose");
 const Joi = require("joi");
-
-const contactSchema = new Schema({
-  name: {
-    type: String,
-    required: [true, "Set name for contact"],
-  },
-  email: {
-    type: String,
-  },
-  phone: {
-    type: String,
-  },
-  favorite: {
-    type: Boolean,
-    default: false,
-  },
-});
-
-contactSchema.post("save", (error, data, next) => {
-  error.status = 400;
-  next();
-});
+const {handleMongooseError} = require("../helpers")
 
 const addSchema = Joi.object({
-  name: Joi.string().required(),
-  email: Joi.string().required(),
-  phone: Joi.string().required(),
-  favorite: Joi.boolean(),
-});
+    name:Joi.string().required(),
+    email:Joi.string().required(),
+    phone:Joi.string().required(),
+    favorite:Joi.boolean()
+})
+
+const updateSchema = Joi.object().keys({
+    name:Joi.string(),
+    email:Joi.string(),
+    phone:Joi.string(),
+    favorite:Joi.boolean(),
+  })
 
 const updateFavoriteSchema = Joi.object({
-  favorite: Joi.boolean().required(),
-});
+    favorite:Joi.boolean().required(),
+})
 
-const Contact = model("contacts", contactSchema);
+const contactSchema = new Schema({
+    
+    name: {
+        type: String,
+        required: [true, 'First Last'],
+    },
+    email: {
+        type: String,
+        required:true,
+    },
+    phone: {
+        type: String,
+        required:true,
+    },
+    favorite: {
+        type: Boolean,
+        default: false,
+    },
+    owner:{
+        type: Schema.Types.ObjectId,
+        ref:"user",
+        required:true,
+    }
+      
+})
 
-const schemas = {
-  addSchema,
-  updateFavoriteSchema,
-};
+const Contact = model("Contact", contactSchema)
+const schema = {addSchema,updateSchema,updateFavoriteSchema};
+contactSchema.post("save",handleMongooseError)
+
 
 module.exports = {
-  Contact,
-  schemas,
+    Contact,
+    schema,
 };
-
-module.exports = { Contact, schemas };
