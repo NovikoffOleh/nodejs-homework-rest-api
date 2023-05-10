2f373aa223a460e310657931cd61520657a3dc24
NovikoffOleh <novikoff10000@gmail.com>
2023-05-10 17:59:55 +0300

    upd files

---
+++ middlewars/upload.js
@@ -0,0 +1,22 @@
+//const express = require("express");
+//const cors = require("cors");
+//const { nanoid } = require("nanoid");
+const multer = require("multer");
+const path = require("path");
+//const fs = require("fs/promises");
+
+
+const tempDir = path.join(__dirname, "../", "temp");
+
+const multerConfig = multer.diskStorage({
+    destination: tempDir,
+    filename: (req, file, cb) => {
+        cb(null, file.originalname);
+    }
+})
+
+const upload = multer({
+    storage: multerConfig
+})
+
+module.exports = upload;
